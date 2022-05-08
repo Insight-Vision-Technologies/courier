961de88af4f9394bc7280f59581df3e0137c77c1
fzain786 <88419680+fzain786@users.noreply.github.com>
2022-05-08 22:49:28 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   Api_fake:'https://6170c99b23781c0017289b07.mockapi.io/company/car/CarInfo',
   Api_identity:'https://localhost:44381/',
-  Api_core:'https://localhost:44346/api'
+  Api_core: 'https://localhost:44346/api',
+  base_core:'https://localhost:44346/images/',
 
 };
