5530dc10897694e6c102e2f973855e974d719ad1
fzain786 <88419680+fzain786@users.noreply.github.com>
2022-05-12 12:44:47 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   Api_fake:'https://6170c99b23781c0017289b07.mockapi.io/company/car/CarInfo',
   Api_identity:'https://localhost:44381/',
-  Api_core: 'https://localhost:44346/api',
-  base_core:'https://localhost:44346/images/',
+  Api_core: 'http://insightvision-001-site1.etempurl.com/api',
+  base_core:'http://insightvision-001-site1.etempurl.com/images/',
 
 };
